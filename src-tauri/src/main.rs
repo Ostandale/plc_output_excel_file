6a3b6b20dcc1c78c32e1d28266e7838182277877
Ostandale <dio7890@gmail.com>
2022-11-07 12:44:05 +0900

    途中停止時間算出機能追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -22,6 +22,20 @@ struct Csv_data {
     work_num: &'static str,
 }
 
+const LOCATE_STD_INFO01_ROW: u32 = 0;
+const LOCATE_STD_INFO01_COL: u16 = 0;
+
+const LOCATE_STD_COUNT01_ROW: u32 = 4;
+const LOCATE_STD_COUNT01_COL: u16 = 8;
+
+const LOCATE_STD_TIME01_ROW: u32 = 4;
+const LOCATE_STD_TIME01_COL: u16 = 0;
+
+const LOCATE_STD_RESIST01_ROW: u32 = 0;
+const LOCATE_STD_RESIST01_COL: u16 = 4;
+
+const LOCATE_STD_CP01_ROW: u32 = 0;
+const LOCATE_STD_CP01_COL: u16 = 8;
 //-------------------------------------------------------------------
 // 関数名：
 // 引数：
@@ -154,6 +168,12 @@ fn read_csv_file(path: &String, file_name: &String) -> Result<(), Box<dyn Error>
         .set_font_color(FormatColor::White)
         .set_bg_color(FormatColor::Black);
 
+    let format_small_num = workbook
+        .add_format()
+        .set_font_color(FormatColor::Black)
+        .set_bg_color(FormatColor::White)
+        .set_num_format("0.000");
+
     let mut sheet1 = workbook.add_worksheet(Some("分析結果")).unwrap();
     let mut sheet2 = workbook.add_worksheet(Some("ログ")).unwrap();
 
@@ -168,10 +188,25 @@ fn read_csv_file(path: &String, file_name: &String) -> Result<(), Box<dyn Error>
     let mut start_time: i64 = 0;
     let mut end_time: i64 = 0;
 
+    //  *   チョコ停計測用
+    let mut stop_last_time: i64 = 0;
+    let mut stop_count: i64 = 0;
+    let mut stop_time_total: i64 = 0;
+
     //  *   CE LO HI 判定用フラグ
     let mut is_ce: bool = false;
     let mut is_lo: bool = false;
     let mut is_hi: bool = false;
+
+    //  CP値算出用
+    let mut cp_total: f64 = 0.0;
+    let mut cp_counter: f64 = 0.0;
+    let mut cp_measured_value: Vec<f64> = Vec::new();
+    let mut cp_differnce: f64 = 0.0;
+    let mut cp_up_value: f64 = 0.0;
+    let mut cp_down_value: f64 = 0.0;
+    let mut most_high_value: f64 = 0.0;
+    let mut most_low_value: f64 = 0.0;
 
     //  *   一番最初の行を抜かして2番目から読込
     //  *   最初の行はheadersで読み込める
@@ -184,17 +219,91 @@ fn read_csv_file(path: &String, file_name: &String) -> Result<(), Box<dyn Error>
 
         if index == 0 {
             //  *   データ取得1回目の情報から号機、製品番号、ロット番号をエクセルシートの1行目に書き込む
-            sheet1.write_string(0, 0, "号機", Some(&format1)).unwrap();
-            sheet1.write_string(1, 0, &rec[1], Some(&format1)).unwrap();
-
-            sheet1.write_string(0, 1, "品番", Some(&format1)).unwrap();
-            sheet1.write_string(1, 1, &rec[2], Some(&format1)).unwrap();
+            sheet1
+                .write_string(
+                    LOCATE_STD_INFO01_ROW,
+                    LOCATE_STD_INFO01_COL,
+                    "号機",
+                    Some(&format1),
+                )
+                .unwrap();
+            sheet1
+                .write_string(
+                    LOCATE_STD_INFO01_ROW + 1,
+                    LOCATE_STD_INFO01_COL,
+                    &rec[1],
+                    Some(&format1),
+                )
+                .unwrap();
 
             sheet1
-                .write_string(0, 2, "ロット番号", Some(&format1))
+                .write_string(
+                    LOCATE_STD_INFO01_ROW + 0,
+                    LOCATE_STD_INFO01_COL + 1,
+                    "品番",
+                    Some(&format1),
+                )
                 .unwrap();
-            sheet1.write_string(1, 2, &rec[3], Some(&format1)).unwrap();
+            sheet1
+                .write_string(
+                    LOCATE_STD_INFO01_ROW + 1,
+                    LOCATE_STD_INFO01_COL + 1,
+                    &rec[2],
+                    Some(&format1),
+                )
+                .unwrap();
+
+            sheet1
+                .write_string(
+                    LOCATE_STD_INFO01_ROW + 0,
+                    LOCATE_STD_INFO01_COL + 2,
+                    "ロット番号",
+                    Some(&format1),
+                )
+                .unwrap();
+            sheet1
+                .write_string(
+                    LOCATE_STD_INFO01_ROW + 1,
+                    LOCATE_STD_INFO01_COL + 2,
+                    &rec[3],
+                    Some(&format1),
+                )
+                .unwrap();
             start_time = *data_epoch_time;
+
+            sheet1
+                .write_string(
+                    LOCATE_STD_RESIST01_ROW + 0,
+                    LOCATE_STD_RESIST01_COL + 0,
+                    "目標抵抗値",
+                    Some(&format1),
+                )
+                .unwrap();
+            sheet1
+                .write_string(
+                    LOCATE_STD_RESIST01_ROW + 1,
+                    LOCATE_STD_RESIST01_COL + 0,
+                    &rec[6],
+                    Some(&format1),
+                )
+                .unwrap();
+
+            sheet1
+                .write_string(
+                    LOCATE_STD_RESIST01_ROW + 0,
+                    LOCATE_STD_RESIST01_COL + 1,
+                    "許容範囲",
+                    Some(&format1),
+                )
+                .unwrap();
+            sheet1
+                .write_string(
+                    LOCATE_STD_RESIST01_ROW + 1,
+                    LOCATE_STD_RESIST01_COL + 1,
+                    &rec[7],
+                    Some(&format1),
+                )
+                .unwrap();
 
             //  *   エクセルシート2枚目の1行目に見出しを加える
             sheet2.write_string(0, 0, "測定値", Some(&format1)).unwrap();
@@ -202,13 +311,57 @@ fn read_csv_file(path: &String, file_name: &String) -> Result<(), Box<dyn Error>
             sheet2
                 .write_string(0, 2, "作業時間", Some(&format1))
                 .unwrap();
+
+            //  CP計算用
+            let difference: f64 = rec[7].parse::<f64>().unwrap();
+            let resistance: f64 = rec[6].parse::<f64>().unwrap();
+            cp_up_value = resistance * (1.0 + difference / 100.0);
+            cp_down_value = resistance * (1.0 - difference / 100.0);
+            cp_differnce = cp_up_value - cp_down_value;
+            let cp_up_round = ((cp_up_value * 100.0).round()) / 100.0;
+            let cp_down_round = ((cp_down_value * 100.0).round()) / 100.0;
+            let cp_diff_width = format!("{}-{}", cp_up_round, cp_down_round);
+
+            sheet1
+                .merge_range(
+                    LOCATE_STD_RESIST01_ROW + 0,
+                    LOCATE_STD_RESIST01_COL + 2,
+                    LOCATE_STD_RESIST01_ROW + 0,
+                    LOCATE_STD_RESIST01_COL + 3,
+                    "許容範囲",
+                    Some(&format1),
+                )
+                .unwrap();
+            sheet1
+                .merge_range(
+                    LOCATE_STD_RESIST01_ROW + 1,
+                    LOCATE_STD_RESIST01_COL + 2,
+                    LOCATE_STD_RESIST01_ROW + 1,
+                    LOCATE_STD_RESIST01_COL + 3,
+                    &cp_diff_width,
+                    Some(&format1),
+                )
+                .unwrap();
+
+            //  チョコ停　最初の１回だけトータルタイムに入れない
+            stop_last_time = *data_epoch_time;
         }
+
+        //  途中停止時間測定
+        if *data_epoch_time > stop_last_time + 10000 {
+            stop_count += 1;
+            stop_time_total += (data_epoch_time - stop_last_time);
+        }
+        stop_last_time = *data_epoch_time;
 
         //  OK CE LO HI その他ごとにカウント　CE　LO　HIに関しては異常が出たら2回判定するのでそのあたりも考慮してカウント
         let col1 = match data_decision {
             "OK" => {
                 ok_counter += 1;
                 product_counter += 1;
+                cp_measured_value.push(rec[4].parse().unwrap());
+                cp_total += &rec[4].parse().unwrap();
+                cp_counter += 1.0;
                 is_ce = false;
                 is_lo = false;
                 is_hi = false;
@@ -224,22 +377,26 @@ fn read_csv_file(path: &String, file_name: &String) -> Result<(), Box<dyn Error>
                 }
                 &format_ce
             }
-            //  todo
-            "LO" => {
+            "LOW" => {
                 if is_lo {
                     lo_counter += 1;
                     product_counter += 1;
+                    cp_measured_value.push(rec[4].parse().unwrap());
+                    cp_total += &rec[4].parse().unwrap();
+                    cp_counter += 1.0;
                     is_lo = false;
                 } else {
                     is_lo = true;
                 }
                 &format_lo
             }
-            //  todo
             "HI" => {
                 if is_hi {
                     hi_counter += 1;
                     product_counter += 1;
+                    cp_measured_value.push(rec[4].parse().unwrap());
+                    cp_total += &rec[4].parse().unwrap();
+                    cp_counter += 1.0;
                     is_hi = false;
                 } else {
                     is_hi = true;
@@ -252,13 +409,14 @@ fn read_csv_file(path: &String, file_name: &String) -> Result<(), Box<dyn Error>
             }
         };
 
+        //  todo    シート２ページ目　ログ書込み
         //  *   判定に合わせて色付をしながら各データを１つづつ書込
         sheet2
             .write_number(
                 (index + 1).try_into().unwrap(),
                 0,
                 *data_measured_value,
-                None,
+                Some(&format_small_num),
             )
             .unwrap();
 
@@ -286,64 +444,339 @@ fn read_csv_file(path: &String, file_name: &String) -> Result<(), Box<dyn Error>
         //       println!("{}  :  {}    :     {}", data_1, data_2, data_3);
     }
 
-    //  *   最後の列で異常が出ていたらカウント
-    if is_ce {
-        ce_counter += 1;
-        product_counter += 1;
-    }
+    // //  *   最後の列で異常が出ていたらカウント
+    // if is_ce {
+    //     ce_counter += 1;
+    //     product_counter += 1;
+    // }
 
     //  *   最終統計の書込
     //  総個数
-    sheet1.write_string(3, 0, "総個数", Some(&format1)).unwrap();
     sheet1
-        .write_number(4, 0, product_counter as f64, Some(&format1))
+        .write_string(
+            LOCATE_STD_COUNT01_ROW,
+            LOCATE_STD_COUNT01_COL + 0,
+            "総個数",
+            Some(&format1),
+        )
+        .unwrap();
+    sheet1
+        .write_number(
+            LOCATE_STD_COUNT01_ROW + 1,
+            LOCATE_STD_COUNT01_COL + 0,
+            product_counter as f64,
+            Some(&format1),
+        )
         .unwrap();
 
     //OK
-    sheet1.write_string(3, 1, "OK", Some(&format_ok)).unwrap();
     sheet1
-        .write_number(4, 1, ok_counter as f64, Some(&format_ok))
+        .write_string(
+            LOCATE_STD_COUNT01_ROW,
+            LOCATE_STD_COUNT01_COL + 1,
+            "OK",
+            Some(&format_ok),
+        )
+        .unwrap();
+    sheet1
+        .write_number(
+            LOCATE_STD_COUNT01_ROW + 1,
+            LOCATE_STD_COUNT01_COL + 1,
+            ok_counter as f64,
+            Some(&format_ok),
+        )
         .unwrap();
 
     //  CE
-    sheet1.write_string(3, 2, "CE", Some(&format_ce)).unwrap();
     sheet1
-        .write_number(4, 2, ce_counter as f64, Some(&format_ce))
+        .write_string(
+            LOCATE_STD_COUNT01_ROW,
+            LOCATE_STD_COUNT01_COL + 2,
+            "CE",
+            Some(&format_ce),
+        )
+        .unwrap();
+    sheet1
+        .write_number(
+            LOCATE_STD_COUNT01_ROW + 1,
+            LOCATE_STD_COUNT01_COL + 2,
+            ce_counter as f64,
+            Some(&format_ce),
+        )
         .unwrap();
 
     //  LO
-    sheet1.write_string(3, 3, "LO", Some(&format_lo)).unwrap();
     sheet1
-        .write_number(4, 3, lo_counter as f64, Some(&format_lo))
+        .write_string(
+            LOCATE_STD_COUNT01_ROW,
+            LOCATE_STD_COUNT01_COL + 3,
+            "LOW",
+            Some(&format_lo),
+        )
+        .unwrap();
+    sheet1
+        .write_number(
+            LOCATE_STD_COUNT01_ROW + 1,
+            LOCATE_STD_COUNT01_COL + 3,
+            lo_counter as f64,
+            Some(&format_lo),
+        )
         .unwrap();
 
     //  HI
-    sheet1.write_string(3, 4, "HI", Some(&format_hi)).unwrap();
     sheet1
-        .write_number(4, 4, hi_counter as f64, Some(&format_hi))
+        .write_string(
+            LOCATE_STD_COUNT01_ROW,
+            LOCATE_STD_COUNT01_COL + 4,
+            "HI",
+            Some(&format_hi),
+        )
+        .unwrap();
+    sheet1
+        .write_number(
+            LOCATE_STD_COUNT01_ROW + 1,
+            LOCATE_STD_COUNT01_COL + 4,
+            hi_counter as f64,
+            Some(&format_hi),
+        )
         .unwrap();
 
     //  エラー
     sheet1
-        .write_string(3, 5, "エラー", Some(&format_etc))
+        .write_string(
+            LOCATE_STD_COUNT01_ROW,
+            LOCATE_STD_COUNT01_COL + 5,
+            "エラー",
+            Some(&format_etc),
+        )
         .unwrap();
     sheet1
-        .write_number(4, 5, etc_counter as f64, Some(&format_etc))
+        .write_number(
+            LOCATE_STD_COUNT01_ROW + 1,
+            LOCATE_STD_COUNT01_COL + 5,
+            etc_counter as f64,
+            Some(&format_etc),
+        )
         .unwrap();
 
-    //  総時間
+    //  時間
+
+    //  総作業時間
     sheet1
-        .write_string(0, 4, "総作業時間", Some(&format1))
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 0,
+            LOCATE_STD_TIME01_COL + 0,
+            "総作業時間",
+            Some(&format1),
+        )
         .unwrap();
     sheet1
         .write_string(
-            1,
-            4,
+            LOCATE_STD_TIME01_ROW + 1,
+            LOCATE_STD_TIME01_COL + 0,
             &chrono::Utc
                 .timestamp((end_time - start_time) / 1000, 0)
                 .format("%H:%M:%S")
                 .to_string(),
             Some(&format1),
+        )
+        .unwrap();
+
+    //  開始時間
+    sheet1
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 0,
+            LOCATE_STD_TIME01_COL + 2,
+            "作業開始時間",
+            Some(&format1),
+        )
+        .unwrap();
+    sheet1
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 1,
+            LOCATE_STD_TIME01_COL + 2,
+            &chrono::Utc
+                .timestamp_millis((start_time + (9 * 3600) * 1000))
+                .format("%H:%M:%S")
+                .to_string(),
+            Some(&format1),
+        )
+        .unwrap();
+
+    //  終了時間
+    sheet1
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 0,
+            LOCATE_STD_TIME01_COL + 4,
+            "作業終了時間",
+            Some(&format1),
+        )
+        .unwrap();
+    sheet1
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 1,
+            LOCATE_STD_TIME01_COL + 4,
+            &chrono::Utc
+                .timestamp_millis((end_time + (9 * 3600) * 1000))
+                .format("%H:%M:%S")
+                .to_string(),
+            Some(&format1),
+        )
+        .unwrap();
+
+    //  チョコ停　カウント
+    let format_stop = match stop_count {
+        0..=2 => &format_ok,
+        2..=6 => &format_lo,
+        6..=10 => &format_hi,
+        _ => &format_ce,
+    };
+    sheet1
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 3,
+            LOCATE_STD_TIME01_COL + 0,
+            "途中停止回数",
+            Some(format_stop),
+        )
+        .unwrap();
+    sheet1
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 4,
+            LOCATE_STD_TIME01_COL + 0,
+            &stop_count.to_string(),
+            Some(format_stop),
+        )
+        .unwrap();
+
+    //  チョコ停　総時間
+    sheet1
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 3,
+            LOCATE_STD_TIME01_COL + 2,
+            "途中停止時間",
+            Some(&format1),
+        )
+        .unwrap();
+    sheet1
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 4,
+            LOCATE_STD_TIME01_COL + 2,
+            &chrono::Utc
+                .timestamp_millis(stop_time_total)
+                .format("%H:%M:%S")
+                .to_string(),
+            Some(&format1),
+        )
+        .unwrap();
+
+    //  １個生産するのにかかる時間
+    sheet1
+        .write_string(
+            LOCATE_STD_TIME01_ROW + 0,
+            LOCATE_STD_TIME01_COL + 6,
+            "タクトタイム",
+            Some(&format1),
+        )
+        .unwrap();
+    sheet1
+        .write_number(
+            LOCATE_STD_TIME01_ROW + 1,
+            LOCATE_STD_TIME01_COL + 6,
+            ((end_time - start_time) as f64 / product_counter as f64) / 1000.0,
+            Some(&format1),
+        )
+        .unwrap();
+
+    //  todo    CP値計算
+    let cp_average = cp_total / (cp_counter); //  平均　有効取得抵抗値の合計を有効数で割った数値
+
+    let mut cp_total_for_distributed: f64 = 0.0; // 分散　（取得抵抗値ー平均値）^2 を有効数で割った数値
+    for index in cp_measured_value {
+        cp_total_for_distributed += (index - cp_average) * (index - cp_average);
+    }
+    let cp_distributed: f64 = cp_total_for_distributed / (cp_counter);
+
+    let standard_deviation = cp_distributed.sqrt(); //  標準偏差　分散の平方根を取った数値
+
+    let cp_value = cp_differnce / (standard_deviation * 6.0); // CP値　目標値±許容差の幅を標準偏差 * 6 で割った数値
+
+    cp_up_value = cp_up_value - cp_average; //  CPK値　平均値と許容差の間で小さい方の数値
+    cp_down_value = cp_average - cp_down_value;
+    let cpk_value: f64 = if cp_up_value > cp_down_value {
+        cp_down_value / (standard_deviation * 3.0) // （目標値＋許容差ー平均値）/標準偏差 * 3
+    } else {
+        cp_up_value / (standard_deviation * 3.0) // （平均値ー目標値ー許容差）/標準偏差 * 3
+    };
+
+    //  平均抵抗値
+    sheet1
+        .write_string(
+            LOCATE_STD_CP01_ROW + 0,
+            LOCATE_STD_CP01_COL + 0,
+            "平均抵抗値",
+            Some(&format1),
+        )
+        .unwrap();
+    sheet1
+        .write_number(
+            LOCATE_STD_CP01_ROW + 1,
+            LOCATE_STD_CP01_COL + 0,
+            cp_average,
+            Some(&format1),
+        )
+        .unwrap();
+
+    // CP値色分け
+    let format_cp = match cp_value {
+        v if v >= 1.67 => &format_ok,
+        v if v >= 1.33 => &format_ok,
+        v if v >= 1.00 => &format_lo,
+        v if v >= 0.67 => &format_hi,
+        _ => &format_ce,
+    };
+
+    //  CP値
+    sheet1
+        .write_string(
+            LOCATE_STD_CP01_ROW + 0,
+            LOCATE_STD_CP01_COL + 2,
+            "CP値",
+            Some(format_cp),
+        )
+        .unwrap();
+    sheet1
+        .write_number(
+            LOCATE_STD_CP01_ROW + 1,
+            LOCATE_STD_CP01_COL + 2,
+            cp_value,
+            Some(format_cp),
+        )
+        .unwrap();
+
+    // CPK値色分け
+    let format_cpk = match cpk_value {
+        v if v >= 1.67 => &format_ok,
+        v if v >= 1.33 => &format_ok,
+        v if v >= 1.00 => &format_lo,
+        v if v >= 0.67 => &format_hi,
+        _ => &format_ce,
+    };
+
+    //  CPK値
+    sheet1
+        .write_string(
+            LOCATE_STD_CP01_ROW + 0,
+            LOCATE_STD_CP01_COL + 3,
+            "CPK値",
+            Some(format_cpk),
+        )
+        .unwrap();
+    sheet1
+        .write_number(
+            LOCATE_STD_CP01_ROW + 1,
+            LOCATE_STD_CP01_COL + 3,
+            cpk_value,
+            Some(format_cpk),
         )
         .unwrap();
 
